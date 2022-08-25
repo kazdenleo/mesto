7626eefd1d2d5521798e8aedd60a08769dd4b2ea
kazdenleo <kazakov-denis@list.ru>
2022-08-25 09:53:26 +0300

    feat: добавить работоспособность кнопке Like

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let editButton = document.querySelector('.profile__edit-button');
-let formElement = document.querySelector(".popup");
-let formExit = document.querySelector(".popup__exit");
+let formElement = document.querySelector('.popup');
+let formExit = document.querySelector('.popup__exit');
 
 function showForm() {
 	formElement.classList.add('popup_opened');
@@ -9,17 +9,21 @@ function showForm() {
 function closeForm() {
 	formElement.classList.remove('popup_opened');
 }
-
 editButton.addEventListener('click', showForm);
 formExit.addEventListener('click', closeForm);
 
+// Постановка и удаление Like на пост
+document.querySelectorAll('.elements__like-img').forEach(item => {
+	item.addEventListener('click', event => {
+		item.classList.toggle('elements__like-img_active')
+	})
+})
 
 let formProfile = document.querySelector('.profile__info');
 let nameInput = formProfile.querySelector('.profile__name');
 let infoInput = formProfile.querySelector('.profile__about');
+let popupInput = document.querySelectorAll('.popup__text');
 
-
-let popupInput = document.querySelectorAll(".popup__text");
 popupInput[0].setAttribute('value', nameInput.textContent);
 popupInput[1].setAttribute('value', infoInput.textContent);
 
@@ -29,5 +33,4 @@ function formSubmitHandler (evt) {
     	infoInput.textContent = popupInput[1].value;
     	closeForm()
 }
-
 formElement.addEventListener('submit', formSubmitHandler); 
---
+++ pages/index.js
@@ -0,0 +1,1 @@
+@import url(../js/script.css);
